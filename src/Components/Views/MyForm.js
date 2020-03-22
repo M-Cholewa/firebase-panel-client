5f67e5537f6ebe477b53d09d40e10893176f5219
BuildTools <unconfigured@null.spigotmc.org>
2020-03-22 16:11:17 +0100

    finished refactoring the majority of form

--- src/Components/Views/MyForm.js
+++ src/Components/Views/MyForm.js
@@ -3,13 +3,11 @@ import ReactDOM from "react-dom";
 import { Select, MenuItem, FormControl, InputLabel, Button, TextField, CircularProgress } from '@material-ui/core';
 import { ExpansionPanel, ExpansionPanelSummary, ExpansionPanelDetails, FormControlLabel, IconButton } from '@material-ui/core';
 import ChipInput from 'material-ui-chip-input'
-import Img from 'react-image'
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import DeleteIcon from '@material-ui/icons/Delete';
 import { red, green } from '@material-ui/core/colors';
 import { withStyles } from '@material-ui/core/styles'
 import Axios from "axios";
-// import { fade } from '@material-ui/core/styles/colorManipulator';
 
 
 const styles = theme => ({
@@ -46,11 +44,10 @@ class MyForm extends Component {
             responseObtained: false,
             success: true,
             buttonMessage: 'Zapisz',
-            czyWymagane: true,
-            dobreOdpowiedzi: [],
-            nodes: [],
+            czyWymagane: props.formValues.czyWymagane,
+            dobreOdpowiedzi: props.formValues.dobreOdpowiedzi,
+            nodes: {},
         }
-        // let nodes = []
     }
 
     handleImageChange = event => {
@@ -62,35 +59,34 @@ class MyForm extends Component {
             }
             fr.readAsDataURL(files[0]);
         }
-
     }
 
     handleFormSubmit = () => {
         if (!this.state.loading) {
             this.setState({
-                // loading: true,
+                loading: true,
                 buttonMessage: 'Czekaj..',
             })
-            this.getFormValues()
 
-            // Axios.post('http://localhost:8080/secure/submitForm', this.getFormValues())
-            //     .then(res => {
-            //         this.setState({
-            //             responseObtained: true,
-            //             success: res.result,
-            //             loading: false,
-            //             buttonMessage: res.response
-            //         })
-            //         let cbMessage = res.result ? "Zapisz" : "Sprawdź dane i spróbuj ponownie"
-            //         setTimeout(() => this.setState(
-            //             {
-            //                 responseObtained: false,
-            //                 buttonMessage: cbMessage
-            //             }
-            //         ), 5000)
-            //     })
+            Axios.post('http://localhost:8080/secure/submitForm', this.getFormValues())
+                .then(res => {
+                    this.setState({
+                        responseObtained: true,
+                        success: res.result,
+                        loading: false,
+                        buttonMessage: res.response
+                    })
+                    let cbMessage = res.result ? "Zapisz" : "Sprawdź dane i spróbuj ponownie"
+                    setTimeout(() => this.setState(
+                        {
+                            responseObtained: false,
+                            buttonMessage: cbMessage
+                        }
+                    ), 5000)
+                })
         }
     }
+
     handleTaskRemove = () => {
         Axios.post('http://localhost:8080/secure/removeTask', { 'key': this.props.taskID })
             .then(res => {
@@ -103,65 +99,50 @@ class MyForm extends Component {
 
     getFormValues = () => {
         let id = this.props.id
-
-        let lokalizacjaDl = document.getElementById("lokalizacjaDl" + id).value
-        let lokalizacjaSzer = document.getElementById("lokalizacjaSzer" + id).value
-        let podpisObrazka = document.getElementById("podpisObrazka" + id).value
-        let podtytul = document.getElementById("podtytul" + id).value
-        let trescZadania = document.getElementById("trescZadania" + id).value
-        let tytul = document.getElementById("tytul" + id).value
-        let wprowadzenieDoZadania = document.getElementById("wprowadzenieDoZadania" + id).value
-        let kolejnoscZadania = document.getElementById("kolejnoscZadania" + id).value
         let file = document.getElementById("outlined-button-file" + id).files[0];
-
         let formData = new FormData();
+
         if (this.props.taskID)
             formData.append('key', this.props.taskID)
         formData.append("czyWymagane", this.state.czyWymagane)
-        formData.append("kolejnoscZadania", kolejnoscZadania)
         formData.append("dobreOdpowiedzi", JSON.stringify(this.state.dobreOdpowiedzi))
-        formData.append("lokalizacjaDl", lokalizacjaDl)
-        formData.append("lokalizacjaSzer", lokalizacjaSzer)
-        formData.append("podpisObrazka", podpisObrazka)
-        formData.append("podtytul", podtytul)
-        formData.append("trescZadania", trescZadania)
-        formData.append("wprowadzenieDoZadania", wprowadzenieDoZadania)
-        formData.append("tytul", tytul)
         formData.append('image', file ? file : this.props.formValues.urlZdjeciaDoZadania);
-        for (var pair of formData.entries()) {
-            console.log(pair[0] + ', ' + pair[1]);
+
+        const { nodes } = this.state
+        if (nodes !== {}) {
+            Object.values(nodes).map((node) => {
+                formData.append(node.name, node.value)
+            })
         }
-        // return formData
+        // for (var pair of formData.entries()) {
+        //     console.log(pair[0] + ', ' + pair[1]);
+        // }
+        return formData
     }
 
     componentDidMount() {
         this.setState({
             labelWidth: ReactDOM.findDOMNode(this.InputLabelRef).offsetWidth
         });
-        this.setState({
-            czyWymagane: this.props.formValues.czyWymagane,
-            dobreOdpowiedzi: this.props.formValues.dobreOdpowiedzi
-        })
     }
+
     addNode = node => {
-        // console.log(node)
-        // this.setState({nodes})
         let nodes = this.state.nodes
-        nodes.push(node)
+        nodes[node.name] = node
         this.setState({ nodes })
-        console.log(this.state.nodes)
-        // input = node;
     }
 
     render() {
         const { loading, nodes } = this.state
         const { formValues, id, isNew, classes } = this.props
-        try {
-            nodes[0].value = formValues.podpisObrazka
-
-        } catch (error) {
-
+        //if it is not empty
+        if (nodes !== {}) {
+            Object.values(nodes).map((node) => {
+                if (formValues[node.name] != null)
+                    node.value = formValues[node.name]
+            })
         }
+
         return (
             <ExpansionPanel
                 className={classes.expPanel}
@@ -217,24 +198,21 @@ class MyForm extends Component {
                                 name="czyWymagane"
                                 labelId="czyWymagane"
                                 onChange={event => this.setState({ czyWymagane: event.target.value })}
-                                // id={"demo-simple-select-outlined" + id}
                                 labelWidth={this.state.labelWidth}
-                                value={this.state.czyWymagane || true}
-                            // value={false}
+                                defaultValue={this.state.czyWymagane}
+                            // inputRef={this.addNode}
                             >
                                 <MenuItem value={true}>Tak</MenuItem>
                                 <MenuItem value={false}>Nie</MenuItem>
                             </Select>
                         </FormControl>
-                        <br />
-                    "dobreOdpowiedzi"<br />
 
+                    "dobreOdpowiedzi"
                         <ChipInput
-                            id={"dobreOdpowiedzi" + id}
-                            defaultValue={formValues.dobreOdpowiedzi}
                             onChange={event => {
                                 this.setState({ dobreOdpowiedzi: event })
                             }}
+                            defaultValue={formValues.dobreOdpowiedzi}
                             variant="outlined"
                             label="dobreOdpowiedzi"
                             style={{
@@ -246,97 +224,94 @@ class MyForm extends Component {
                             margin="normal"
                         />
 
-                    "lokalizacjaDl"<br />
+                    "lokalizacjaDl"
                         <TextField
-                            id={"lokalizacjaDl" + id}
-                            defaultValue={formValues.lokalizacjaDl}
-                            inputProps={{
-                                step: "0.01",
-                                lang: "en"
-                            }}
                             variant="outlined"
                             label="lokalizacjaDl"
-                            style={{ margin: 8 }}
-                            name="lokalizacjaDl"
-                            type="number"
-                            required
-                            margin="normal"
-                        />
-                    "lokalizacjaSzer"<br />
-                        <TextField
-                            id={"lokalizacjaSzer" + id}
-                            defaultValue={formValues.lokalizacjaSzer}
-                            variant="outlined"
-                            label="lokalizacjaSzer"
-                            style={{ margin: 8 }}
-                            name="lokalizacjaSzer"
-                            type="number"
-                            required
-                            margin="normal"
-                        />
-                    "podpisObrazka"<br />
-                        <TextField
-                            id={"podpisObrazka" + id}
-                            // defaultValue={formValues.podpisObrazka}
-                            variant="outlined"
-                            label="podpisObrazka"
-                            // ref="podpisObrazka"
                             inputRef={this.addNode}
                             style={{ margin: 8 }}
+                            name="lokalizacjaDl"
                             margin="normal"
+                            inputProps={{
+                                step: "0.000001",
+                                lang: "en"
+                            }}
+                            type="number"
+                            required
                         />
-                    "kolejnoscZadania"<br />
+                    "lokalizacjaSzer"
                         <TextField
-                            id={"kolejnoscZadania" + id}
-                            defaultValue={formValues.kolejnoscZadania}
+                            variant="outlined"
+                            label="lokalizacjaSzer"
+                            inputRef={this.addNode}
+                            style={{ margin: 8 }}
+                            name="lokalizacjaSzer"
+                            margin="normal"
+                            inputProps={{
+                                step: "0.000001",
+                                lang: "en"
+                            }}
+                            type="number"
+                            required
+                        />
+                    "kolejnoscZadania"
+                        <TextField
                             variant="outlined"
                             label="kolejnoscZadania"
+                            inputRef={this.addNode}
                             style={{ margin: 8 }}
-                            required
+                            name="kolejnoscZadania"
                             margin="normal"
-                        />
-                    "podtytul"<br />
-                        <TextField
                             required
-                            id={"podtytul" + id}
-                            defaultValue={formValues.podtytul}
+                        />
+                    "podtytul"
+                        <TextField
                             variant="outlined"
                             label="podtytul"
+                            inputRef={this.addNode}
                             style={{ margin: 8 }}
                             name="podtytul"
                             margin="normal"
+                            required
                         />
-                    "trescZadania"<br />
+                    "trescZadania"
                         <TextField
-                            id={"trescZadania" + id}
-                            defaultValue={formValues.trescZadania}
                             variant="outlined"
                             label="trescZadania"
-                            required
+                            inputRef={this.addNode}
                             style={{ margin: 8 }}
                             name="trescZadania"
                             margin="normal"
+                            required
                         />
                     "tytul"
                     <TextField
-                            id={"tytul" + id}
-                            defaultValue={formValues.tytul}
                             variant="outlined"
                             label="tytul"
-                            required
+                            inputRef={this.addNode}
                             style={{ margin: 8 }}
                             name="tytul"
                             margin="normal"
+                            required
                         />
+
                     wprowadzenieDoZadania
                     <TextField
-                            id={"wprowadzenieDoZadania" + id}
-                            defaultValue={formValues.wprowadzenieDoZadania}
                             variant="outlined"
                             label="wprowadzenieDoZadania"
-                            required
+                            inputRef={this.addNode}
                             style={{ margin: 8 }}
                             name="wprowadzenieDoZadania"
+                            margin="normal"
+                            required
+                        />
+                    "podpisObrazka"
+                        <TextField
+                            variant="outlined"
+                            label="podpisObrazka"
+                            inputRef={this.addNode}
+                            style={{ margin: 8 }}
+                            name="podpisObrazka"
                             margin="normal"
                         />
                     "urlZdjeciaDoZadania"
@@ -358,7 +333,7 @@ class MyForm extends Component {
 
                     </Button>
                         </label>
-                        <Img
+                        <img
                             id={"imgSpotlight" + id}
                             src={formValues.urlZdjeciaDoZadania}
                             style={{ width: "20%", height: "20%", margin: "auto" }}
